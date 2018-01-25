c3d31e4a6c17ea45333ec115841b0142a2fa2c8f
Libin Lu <llu@evolenthealth.com>
2018-01-25 10:01:26 -0500

    update work directory

--- tasks/xdt.js
+++ tasks/xdt.js
@@ -12,9 +12,9 @@ var workingFolder = task.getInput('workingFolder') || task.cwd();
 var lines = input.split(/\r?\n/);
 for(var i=0; i<lines.length; i++){
     var items = lines[i].split(/\s*=>\s*/);
-    var xml = items[1];
-    var xdt = items[0];
-    var output = items.length >=3 ? items[2] : xml;
+    var xml = path.join(workingFolder, items[1]);
+    var xdt = path.join(workingFolder, items[0]);
+    var output = items.length >=3 ? path.join(workingFolder, items[2]) : xml;
     exec("dotnet dotnet-transform-xdt.dll -x " + xml + " -o " + output + " -t " + xdt, function (err, stdout, stderr) {
         if (err) {
             throw new Error(err);
