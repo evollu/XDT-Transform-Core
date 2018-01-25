e7d96487c55b83ba3887050ef74016ca0d8dad55
Libin Lu <llu@evolenthealth.com>
2018-01-25 09:59:01 -0500

    add multiple line support

--- tasks/xdt.js
+++ tasks/xdt.js
@@ -2,17 +2,28 @@
 exports.__esModule = true;
 var task = require("vsts-task-lib");
 var path = require("path");
-task.setResourcePath(path.join(__dirname, 'task.json'));
-var xml = task.getInput('xml');
-var output = task.getInput('output');
-var xdt = task.getInput('xdt');
 var exec = require('child_process').exec;
-exec("dotnet dotnet-transform-xdt.dll -x " + xml + " -o " + output + " -t " + xdt, function (err, stdout, stderr) {
-    if (err) {
-        // node couldn't execute the command
-        return;
-    }
-    // the *entire* stdout and stderr (buffered)
-    console.log(stdout);
-    console.error(stderr);
-});
+
+task.setResourcePath(path.join(__dirname, 'task.json'));
+
+var input = task.getInput('transforms', true);
+var workingFolder = task.getInput('workingFolder') || task.cwd();
+
+var lines = input.split(/\r?\n/);
+for(var i=0; i<lines.length; i++){
+    var items = lines[i].split(/\s*=>\s*/);
+    var xml = items[1];
+    var xdt = items[0];
+    var output = items.length >=3 ? items[2] : xml;
+    exec("dotnet dotnet-transform-xdt.dll -x " + xml + " -o " + output + " -t " + xdt, function (err, stdout, stderr) {
+        if (err) {
+            throw new Error(err);
+            return;
+        }
+        console.log(stdout);
+        if(stderr){
+            throw new Error(stderr);
+        }
+    });
+}
+
